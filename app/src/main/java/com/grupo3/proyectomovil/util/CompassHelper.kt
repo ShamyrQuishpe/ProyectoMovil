079093bab4b1e95e11204074c37e520b23c3a15b
Miguel <miguel.carvajal@epn.edu.ec>
2025-01-25 20:03:52 -0500

    CompassHelper.kt

--- app/src/main/java/com/grupo3/proyectomovil/util/CompassHelper.kt
+++ app/src/main/java/com/grupo3/proyectomovil/util/CompassHelper.kt
@@ -1,4 +1,69 @@
 package com.grupo3.proyectomovil.util
 
-class CompassHelper {
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import io.github.sceneview.math.Rotation
+
+class CompassHelper (private val context: Context){
+    private val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    private var onRotationChanged: ((Rotation) -> Unit)? = null
+    private val accelerometerReading = FloatArray(3)
+    private val magnetometerReading = FloatArray(3)
+    private val rotationMatrix = FloatArray(9)
+    private val orientationAngles = FloatArray(3)
+
+    fun start(onRotationChanged: (Rotation) -> Unit) {
+        this.onRotationChanged = onRotationChanged
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
+            sensorManager.registerListener(
+                sensorEventListener,
+                accelerometer,
+                SensorManager.SENSOR_DELAY_GAME
+            )
+        }
+        sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)?.also { magneticField ->
+            sensorManager.registerListener(
+                sensorEventListener,
+                magneticField,
+                SensorManager.SENSOR_DELAY_GAME
+            )
+        }
+    }
+
+    fun stop() {
+        sensorManager.unregisterListener(sensorEventListener)
+    }
+
+    private val sensorEventListener = object : SensorEventListener {
+        override fun onSensorChanged(event: SensorEvent) {
+            when (event.sensor.type) {
+                Sensor.TYPE_ACCELEROMETER -> {
+                    System.arraycopy(event.values, 0, accelerometerReading, 0, accelerometerReading.size)
+                }
+                Sensor.TYPE_MAGNETIC_FIELD -> {
+                    System.arraycopy(event.values, 0, magnetometerReading, 0, magnetometerReading.size)
+                }
+            }
+            updateOrientationAngles()
+        }
+
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+    }
+
+    private fun updateOrientationAngles() {
+        SensorManager.getRotationMatrix(
+            rotationMatrix,
+            null,
+            accelerometerReading,
+            magnetometerReading
+        )
+        SensorManager.getOrientation(rotationMatrix, orientationAngles)
+        val azimuth = Math.toDegrees(orientationAngles[0].toDouble()).toFloat()
+        onRotationChanged?.invoke(Rotation(0f, azimuth, 0f))
+    }
+
 }
