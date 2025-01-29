ffbf628721138b6729b706e77604001f5d793597
Monica Jaña <monicajana1804@gmail.com>
2025-01-28 19:45:10 -0500

    update facultyData

--- app/src/main/java/com/grupo3/proyectomovil/data/FacultyData.kt
+++ app/src/main/java/com/grupo3/proyectomovil/data/FacultyData.kt
@@ -32,7 +32,7 @@ object FacultyData {
             detectionRange = 20f
         ),
         Faculty(
-            id = 2,
+            id = 4,
             name = "Facultad de Ciencias Administrativas",
             description = "Edificio 25 - Facultad de Ingeniería en Sistemas",
             latitude = -0.21020,
@@ -41,7 +41,7 @@ object FacultyData {
             detectionRange = 20f
         ),
         Faculty(
-            id = 3,
+            id = 5,
             name = "Facultad de Ingeniería Civil y Ambiental",
             description = "Edificio 6 - Facultad de Ingeniería Civil y Ambiental",
             latitude = -0.20939,
@@ -50,7 +50,7 @@ object FacultyData {
             detectionRange = 20f
         ),
         Faculty(
-            id = 3,
+            id = 6,
             name = "Facultad de Ingeniería Mecánica ",
             description = "Edificio 15 - Facultad de Ingeniería Mecánica",
             latitude = -0.20939,
@@ -59,7 +59,7 @@ object FacultyData {
             detectionRange = 20f
         ),
         Faculty(
-            id = 3,
+            id = 7,
             name = "Facultad de Ingeniería Eléctrica y Electrónica",
             description = "Edificio 16 - Facultad de Ingeniería Eléctrica y Electrónica",
             latitude = -0.20939,
@@ -68,7 +68,7 @@ object FacultyData {
             detectionRange = 20f
         ),
         Faculty(
-            id = 3,
+            id = 8,
             name = "Facultad de Geología y Petróleos",
             description = "Edificio 13 - Facultad de Geología y Petróleos",
             latitude = -0.20939,
