4753bbf135146f1072cf311588eb010194222d7a
shamy <shamyrquishpet@gmail.com>
2025-01-30 20:42:54 -0500

    update facultydata

--- app/src/main/java/com/grupo3/proyectomovil/data/FacultyData.kt
+++ app/src/main/java/com/grupo3/proyectomovil/data/FacultyData.kt
@@ -16,10 +16,10 @@ object FacultyData {
         Faculty(
             id = 2,
             name = "Facultad de Sistemas",
-            description = "Facultad de Ingeniería en Sistemas",
+            description = "Edificio 20 - Facultad de Ingeniería en Sistemas",
             latitude = -0.21020,
             longitude = -78.48895,
-            modelPath = "models/FQuimica.glb", // Cambiar el modelo
+            modelPath = "models/sistemas.glb",
             detectionRange = 20f
         ),
         Faculty(
@@ -28,7 +28,7 @@ object FacultyData {
             description = "Edificio 12 - Facultad de Ciencias",
             latitude = -0.20939,
             longitude = -78.48954,
-            modelPath = "models/FQuimica.glb", //Cambiar el modelo
+            modelPath = "models/ciencias.glb",
             detectionRange = 20f
         ),
         Faculty(
@@ -37,43 +37,61 @@ object FacultyData {
             description = "Edificio 25 - Facultad de Ingeniería en Sistemas",
             latitude = -0.21020,
             longitude = -78.48895,
-            modelPath = "models/FQuimica.glb", // Cambiar el modelo
+            modelPath = "models/fca.glb",
             detectionRange = 20f
         ),
         Faculty(
             id = 5,
             name = "Facultad de Ingeniería Civil y Ambiental",
             description = "Edificio 6 - Facultad de Ingeniería Civil y Ambiental",
-            latitude = -0.20939,
-            longitude = -78.48954,
-            modelPath = "models/FQuimica.glb", //Cambiar el modelo
+            latitude = -0.211815,
+            longitude = -78.49131,
+            modelPath = "models/civil.glb",
             detectionRange = 20f
         ),
         Faculty(
             id = 6,
             name = "Facultad de Ingeniería Mecánica ",
             description = "Edificio 15 - Facultad de Ingeniería Mecánica",
-            latitude = -0.20939,
-            longitude = -78.48954,
-            modelPath = "models/FQuimica.glb", //Cambiar el modelo
+            latitude = -0.20954,
+            longitude = -78.48982,
+            modelPath = "models/Mecánica.glb",
             detectionRange = 20f
         ),
         Faculty(
             id = 7,
             name = "Facultad de Ingeniería Eléctrica y Electrónica",
             description = "Edificio 16 - Facultad de Ingeniería Eléctrica y Electrónica",
-            latitude = -0.20939,
-            longitude = -78.48954,
-            modelPath = "models/FQuimica.glb", //Cambiar el modelo
+            latitude = -0.20922,
+            longitude = -78.48950,
+            modelPath = "models/fiee.glb",
             detectionRange = 20f
         ),
         Faculty(
             id = 8,
             name = "Facultad de Geología y Petróleos",
             description = "Edificio 13 - Facultad de Geología y Petróleos",
-            latitude = -0.20939,
-            longitude = -78.48954,
-            modelPath = "models/FQuimica.glb", //Cambiar el modelo
+            latitude = -0.21076,
+            longitude = -78.48949,
+            modelPath = "models/Geología.glb",
+            detectionRange = 20f
+        ),
+        Faculty(
+            id = 9,
+            name = "ESFOT",
+            description = "Edificio 21 - Escuela de formación de tecnologos",
+            latitude = -0.21050,
+            longitude = -78.48844,
+            modelPath = "models/ESFOT.glb",
+            detectionRange = 20f
+        ),
+        Faculty(
+            id = 10,
+            name = "DFB",
+            description = "Edificio 14 - Departamento de formación básica",
+            latitude = -0.20998,
+            longitude = -78.490167,
+            modelPath = "models/DFB.glb",
             detectionRange = 20f
         ),
 
